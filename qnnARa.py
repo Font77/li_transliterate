ee4e58ee1506ad415e1cd53e400492b2bdcd20cf
Font77 <vimalkg@gmail.com>
2017-04-10 09:32:50 +0530

    Added files for all languages

--- qnnARa.py
+++ qnnARa.py
@@ -8,16 +8,17 @@ class qNNARa(object):
     ''' 
     classdocs               
     '''
-    qNNARa_char_tuple = (
+    qNNARa_vovl_tuple = (
         u'ಾ',u'ಅ',u'ಆ', # aAa
         u'ಿ', u'ೀ', u'ಇ', u'ಈ',# iiii
         u'ು', u'ೂ', u'ಉ', u'ಊ',# uuuu
         u'ೆ', u'ೇ', u'ೈ', u'ಎ', u'ಏ', u'ಐ', u'ೖ',# eeeeeee 7 times
         u'ೊ', u'ೋ', u'ೌ', u'ಒ', u'ಓ', u'ಔ',# oooooo 6 times
-        u'ಕ',# q
-        u'ಕ',# q
+        u'ಙ',# N
     )
-
+	qNNARa_qonsts = u'ಕಖಗಘ'
+	iqNNARa_qonsts = 'qQgGcCjJtTdDxXwWn
+	
     def __init__(self, params):
         '''
         Constructor
