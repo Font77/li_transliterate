23fb0e15172c29f4fbe12d5df22c85b3f73212c6
Font77 <vimalkg@gmail.com>
2017-04-12 07:53:08 +0530

    Added files for all indiq languages

--- unicode_teqst.py
+++ unicode_teqst.py
@@ -11,29 +11,49 @@ class unicode_teqst(object):
     '''
     classdocs
     '''
-    mlyalmstr = (u'കഖഗഘ' + u'ങങ' + u'ചഛജഝഞ' + u'ടഠഡഢണ' +
-                u'തഥദധന' + u'പഫബഭമ' + u'യരറലളഴ' +
-                u'വശഷസഹ' + u'ാഅആ' + u'ിീഇഈ' + u'ുൂഉഊ' +
-                u'െേൈഎ' + u'ഏഐee' + u'ൊോൌഒഓഔ')
-    inglisstr = ('qQgG' + 'NN' + 'cCjJn' + 'tTdDn' +
+    inglisstr = ('qQgG' + 'cCjJn' + 'tTdDn' +
                 'xXwWn' + 'pfbBm' + 'yrRlLl' +
                 'vSssh' + 'aAa' + 'iiii' + 'uuuu' +
-                'eeee' + 'eeee' + 'ooo' + 'ooo' + 'jf')
-    hindistr = (u'कखगघ' + u'ंं' + u'चछजझञ' + u'टठडढण'
+                'eeee' + 'eeee' + 'ooo' + 'ooo' + 'NNNN' + 'jf')	
+    mlyalmstr = (u'കഖഗഘ' + u'ചഛജഝഞ' + u'ടഠഡഢണ' +
+                u'തഥദധന' + u'പഫബഭമ' + u'യരറലളഴ' +
+                u'വശഷസഹ' + u'ാഅആ' + u'ിീഇഈ' + u'ുൂഉഊ' +
+                u'െേൈഎ' + u'ഏഐee' + u'ൊോൌഒഓഔ' + u'ങങNN')
+    bNgalistr = (u'কখগঘ' + u'চছজঝঞ' + u'টঠডঢণ' +
+                u'তথদধন' + u'পফবভম' + u'যরRলLl' +
+                u'vশষসহ' + u'াঅআ' + u'িীইঈ' + u'ুূউঊ' +
+                u'েৈএঐ' + u'eeee' + u'োৌওঔৗo' + u'ঙNNN')
+    uRiyastr = (u'କଖଗଘ' + u'ଚଛଜଝଞ' + u'ଟଠଡଢଣ' +
+                u'ତଥଦଧନ' + u'ପଫବଭମ' + u'ଯରRଲଳl' +
+                u'ଵଶଷସହ' + u'ାଅଆ' + u'ିୀଇଈ' + u'ୁୂଉଊ' +
+                u'ୖେଏଐ ' + u'ୈeee' + u'ୗୌୋଓଔo' + u'ଙNNN')
+    pnjabistr = (u'ਕਖਗਘ' + u'ਚਛਜਝਞ' + u'ਟਠਡਢਣ' +
+                u'ਤਥਦਧਨ' + u'ਪਫਬਭਮ' + u'ਯਰRਲਲ਼l' +
+                u'ਵਸ਼sਸਹ' + u'ਾਅਆ' + u'ਿੀੲi' + u'ੁੂੳu' +
+                u'ੇੈਏਐ' + u'eeee' + u'ੋੌਓਔoo' + u'ਙNNN')
+    gujraxistr = (u'કખગઘ' + u'ચછજઝઞ' + u'ટઠડઢણ' +
+                u'તથદધન' + u'પફબભમ' + u'યરRલળl' +
+                u'વશષસહ' + u'ાઅઆ' + u'િીઇઈ' + u'ુૂઉઊ' +
+                u'ેૈએઐ' + u'ૅઍee' + u'ોૌૉઓઔઑ' + u'ઙNNN')
+    xmilstr = (u'கQgG' + u'ஙன' + u'சCஜJஞ' + u'டTdDண' +
+                u'தXwWந' + u'பfbBம' + u'யரறலளழ' +
+                u'வஶஷஸஹ' + u'ாஅஆ' + u'ிீஇஈ' + u'ுூஉஊ' +
+                u'ெேைஎஏஐee' + u'ொோௌஓஔ' + u'ௗ' + u'்ஙனN')
+    hinwistr = (u'कखगघ' + u'चछजझञ' + u'टठडढण'
                 u'तथदधन'  + u'पफबभम' + u'यरड़लळऴ' +
                 u'वशषसह' +u'ाअआ' + u'िीइई' + u'ुूउऊ' +
-                u'ेैएऐ'+ u'ॆएऍऎ' + u'ोौओऔ' + u'ॊऒ' + u'ज़फ़'
+                u'ेैएऐ'+ u'ॆएऍऎ' + u'ोौओऔ' + u'ॊऒ' + u'ंंNN' + u'ज़फ़'
                 )
-    xelgustr = (u'కఖగఘ' + u'ంఙ' + u'చఛజఝఞ' + u'టఠడఢణ' +
+    xelugustr = (u'కఖగఘ' + u'చఛజఝఞ' + u'టఠడఢణ' +
                 u'తథదధన' + u'పఫబభమ' + u'యరఱలళఌ' + u'వశసషహ' +
                 u'ాఅఆ' + u'ిీఇఈ' +u'ుూఉఊ' +
-                u'ెేైఎ' + u'ఏఐ' + u'ౖె' + u'ొోౌఒఓఔ'
+                u'ెేైఎ' + u'ఏఐ' + u'ౖె' + u'ొోౌఒఓఔ' + u'ంఙNN'
                 )
-    qnnARastr = (u'ಕಖಗಘ' + u'ಙಙ' + u'ಚಛಜಝಞ' + u'ಟಠಡಢಣ' +
+    qnARastr = (u'ಕಖಗಘ' + u'ಚಛಜಝಞ' + u'ಟಠಡಢಣ' +
 				u'ತಥದಧನ' + u'ಪಫಬಭಮ' + u'ಯರಱಳಌೡ' +
 				u'ವಶಷಸಹ' + u'ಆಅಾ' + u'ಿೀಇಈ' + u'ುೂಉಊ' +
-				u'ೆೇೈಎ' + u'ಏಏಐೖ' + u'ೊೋೌಒಓಔ')
-				
+				u'ೆೇೈಎ' + u'ಏಏಐೖ' + u'ೊೋೌಒಓಔ' + u'ಙಙNN')
+
     def __init__(self, uteqst):
         '''
         Constructor
@@ -136,23 +156,157 @@ class unicode_teqst(object):
             return translate(from_list, to_list, source)
 
     def hinwitr(self):            
-        hinwiteqst = self.tr(self.hindistr, self.inglisstr, self.uteqst)
+        hinwiteqst = self.tr(self.hinwistr, self.inglisstr, self.uteqst)
         hinwiteqst = hinwiteqst.replace(u'्','')
         hinwiteqst = hinwiteqst.replace(u'़','')
-#         hinwiteqst = hinwiteqst.replace(u'ं','')
         return hinwiteqst
-
-    def xelgutr(self):        
-        xelguteqst = self.tr(self.xelgustr, self.inglisstr, self.uteqst)
-        xelguteqst = xelguteqst.replace(u'్','')
-        return xelguteqst
+    def gujraxitr(self):            
+        gujraxiteqst = self.tr(self.gujraxistr, self.inglisstr, self.uteqst)
+        gujraxiteqst = gujraxiteqst.replace(u'्','')
+        gujraxiteqst = gujraxiteqst.replace(u'़','')
+        return gujraxiteqst
+    def pnjabitr(self):            
+        pnjabiteqst = self.tr(self.pnjabistr, self.inglisstr, self.uteqst)
+        pnjabiteqst = pnjabiteqst.replace(u'्','')
+        pnjabiteqst = pnjabiteqst.replace(u'़','')
+        return pnjabiteqst
+    def bNgalitr(self):            
+        bNgaliteqst = self.tr(self.bNgalistr, self.inglisstr, self.uteqst)
+        bNgaliteqst = bNgaliteqst.replace(u'्','')
+        bNgaliteqst = bNgaliteqst.replace(u'़','')
+        return bNgaliteqst
+    def uRiyatr(self):            
+        uRiyateqst = self.tr(self.uRiyastr, self.inglisstr, self.uteqst)
+        uRiyateqst = uRiyateqst.replace(u'्','')
+        uRiyateqst = uRiyateqst.replace(u'़','')
+        return uRiyateqst
+    def xmiltr(self):        
+        xmilteqst = self.tr(self.xmilstr, self.inglisstr, self.uteqst)
+        xmilteqst = xmilteqst.replace(u'','') #
+        return xmilteqst
+    def xelugutr(self):        
+        xeluguteqst = self.tr(self.xelugustr, self.inglisstr, self.uteqst)
+        xeluguteqst = xeluguteqst.replace(u'్','')
+        return xeluguteqst
     def mlyalmtr(self):        
         mlyalmteqst = self.tr(self.mlyalmstr, self.inglisstr, self.uteqst)
         mlyalmteqst = mlyalmteqst.replace(u'്','')
         return mlyalmteqst
-    def qnnRatr(self):        
-        qnnRateqst = self.tr(self.qnnARastr, self.inglisstr, self.uteqst)
-        qnnRateqst = qnnRateqst.replace(u'್','')
-        qnnRateqst = qnnRateqst.replace(u'ೃ','')
-        qnnRateqst = qnnRateqst.replace(u'ಂ','')
-        return qnnRateqst
+    def qnARatr(self):        
+        qnARateqst = self.tr(self.qnARastr, self.inglisstr, self.uteqst)
+        qnARateqst = qnARateqst.replace(u'್','')
+        qnARateqst = qnARateqst.replace(u'ೃ','')
+        qnARateqst = qnARateqst.replace(u'ಂ','')
+        return qnARateqst
+
+    def ditAqttr(self):
+        trfnq = None        
+        qaunt = 0
+        if self.uteqst[0] in self.hinwistr:
+            qaunt = qaunt + 1
+        if self.uteqst[1] in self.hinwistr:
+            qaunt = qaunt + 1
+        if self.uteqst[2] in self.hinwistr:
+            qaunt = qaunt + 1
+        if self.uteqst[3] in self.hinwistr:
+            qaunt = qaunt + 1
+        if qaunt >= 2:
+            trfnq = self.hinwitr
+
+        qaunt = 0
+        if self.uteqst[0] in self.gujraxistr:
+            qaunt = qaunt + 1
+        if self.uteqst[1] in self.gujraxistr:
+            qaunt = qaunt + 1
+        if self.uteqst[2] in self.gujraxistr:
+            qaunt = qaunt + 1
+        if self.uteqst[3] in self.gujraxistr:
+            qaunt = qaunt + 1
+        if qaunt >= 2:
+            trfnq = self.gujraxitr
+
+        qaunt = 0
+        if self.uteqst[0] in self.pnjabistr:
+            qaunt = qaunt + 1
+        if self.uteqst[1] in self.pnjabistr:
+            qaunt = qaunt + 1
+        if self.uteqst[2] in self.pnjabistr:
+            qaunt = qaunt + 1
+        if self.uteqst[3] in self.pnjabistr:
+            qaunt = qaunt + 1
+        if qaunt >= 2:
+            trfnq = self.pnjabitr
+
+        qaunt = 0
+        if self.uteqst[0] in self.bNgalistr:
+            qaunt = qaunt + 1
+        if self.uteqst[1] in self.bNgalistr:
+            qaunt = qaunt + 1
+        if self.uteqst[2] in self.bNgalistr:
+            qaunt = qaunt + 1
+        if self.uteqst[3] in self.bNgalistr:
+            qaunt = qaunt + 1
+        if qaunt >= 2:
+            trfnq = self.bNgalitr
+
+        qaunt = 0
+        if self.uteqst[0] in self.uRiyastr:
+            qaunt = qaunt + 1
+        if self.uteqst[1] in self.uRiyastr:
+            qaunt = qaunt + 1
+        if self.uteqst[2] in self.uRiyastr:
+            qaunt = qaunt + 1
+        if self.uteqst[3] in self.uRiyastr:
+            qaunt = qaunt + 1
+        if qaunt >= 2:
+            trfnq = self.uRiyatr
+
+        qaunt = 0
+        if self.uteqst[0] in self.xmilstr:
+            qaunt = qaunt + 1
+        if self.uteqst[1] in self.xmilstr:
+            qaunt = qaunt + 1
+        if self.uteqst[2] in self.xmilstr:
+            qaunt = qaunt + 1
+        if self.uteqst[3] in self.xmilstr:
+            qaunt = qaunt + 1
+        if qaunt >= 2:
+            trfnq = self.xmiltr
+
+        qaunt = 0
+        if self.uteqst[0] in self.xelugustr:
+            qaunt = qaunt + 1
+        if self.uteqst[1] in self.xelugustr:
+            qaunt = qaunt + 1
+        if self.uteqst[2] in self.xelugustr:
+            qaunt = qaunt + 1
+        if self.uteqst[3] in self.xelugustr:
+            qaunt = qaunt + 1
+        if qaunt >= 2:
+            trfnq = self.xelugutr
+
+        qaunt = 0
+        if self.uteqst[0] in self.mlyalmstr:
+            qaunt = qaunt + 1
+        if self.uteqst[1] in self.mlyalmstr:
+            qaunt = qaunt + 1
+        if self.uteqst[2] in self.mlyalmstr:
+            qaunt = qaunt + 1
+        if self.uteqst[3] in self.mlyalmstr:
+            qaunt = qaunt + 1
+        if qaunt >= 2:
+            trfnq = self.mlyalmtr
+
+        qaunt = 0
+        if self.uteqst[0] in self.qnARastr:
+            qaunt = qaunt + 1
+        if self.uteqst[1] in self.qnARastr:
+            qaunt = qaunt + 1
+        if self.uteqst[2] in self.qnARastr:
+            qaunt = qaunt + 1
+        if self.uteqst[3] in self.qnARastr:
+            qaunt = qaunt + 1
+        if qaunt >= 2:
+            trfnq = self.qnARatr
+
+        return trfnq()
